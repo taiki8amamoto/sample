0adec98412c261ee797e6ef71ac560f023cf008f
taiki yamamoto <ultra0115.20@gmail.com>
2023-07-12 18:05:16 +0900

    Rails入門27まで完了

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,6 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update]
+
   def index
     @blogs = Blog.all
   end
@@ -8,17 +10,35 @@ class BlogsController < ApplicationController
   end
 
   def create
-    Blog.create(blog_params)
-    redirect_to new_blog_path
+    @blog = Blog.create(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: "ブログを作成しました！"
+    else
+      render :new
+    end
   end
 
   def show
-    @blog = Blog.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
   end
 
   private
 
   def blog_params
     params.require(:blog).permit(:title, :content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
